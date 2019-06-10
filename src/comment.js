db90f0a18f8d19699df9b6ad3305a5047eb28dc9
anw0514 <anw0514@gmail.com>
2019-06-10 09:53:21 -0400

    comments show in correct div

--- src/comment.js
+++ src/comment.js
@@ -1,10 +1,34 @@
 class Comment {
-    constructor() {
+    constructor({author, id, quote}) {
+        this.author = author
+        this.id = id
+        this.quote = quote
+        Comment.all.push(this)
+    }
 
+    render(container) {
+        console.log(this)
+        const com = document.createElement('div')
+        com.innerHTML = this.quote
+        container.appendChild(com)
     }
 
     static getComments() {
         fetch('http://localhost:3000/quotes').then(resp => resp.json())
-        .then(comments => console.log(comments))
+        .then(comments => {
+            comments.forEach(com => {
+                new Comment(com)
+            })
+            this.populateComments()
+        })
+    }
+
+    static populateComments() {
+        const comList = document.getElementById('quote-list')
+        Comment.all.forEach(com => {
+            com.render(comList)
+        })
     }
 }
+
+Comment.all = []
